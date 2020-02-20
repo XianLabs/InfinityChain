e6fe4be6b8be0cdd6aa13d541d7c13bb9049a36e
XianLabs <44497570+XianLabs@users.noreply.github.com>
2020-02-20 02:46:24 -0500

    Update NodeServer.h

--- NodeServer.h
+++ NodeServer.h
@@ -1,19 +1,28 @@
 #ifndef NODESERVER_H
 #define NODESERVER_H
-#include <WinSock2.h>
-#include <string>
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 
 using namespace std;
 
 class NodeServer
 {
+	unsigned short nPort;
+	SOCKET Socket, NewSocket;
+	struct sockaddr_in serv_addr, cli_addr;
 
 private:
-
+	
 
 
 public:
-
+	NodeServer(unsigned short nPort);
+	BOOL Listen();
 };
 
 
