43207461fe9c3fdf34e2b21fe8ca45a219838254
XianLabs <44497570+XianLabs@users.noreply.github.com>
2020-02-19 22:48:40 -0500

    Add files via upload

---
+++ NodeServer.cpp
@@ -0,0 +1,1 @@
+#include "NodeServer.h"
---
+++ NodeServer.h
@@ -0,0 +1,20 @@
+#ifndef NODESERVER_H
+#define NODESERVER_H
+#include <WinSock2.h>
+#include <string>
+
+using namespace std;
+
+class NodeServer
+{
+
+private:
+
+
+
+public:
+
+};
+
+
+#endif
